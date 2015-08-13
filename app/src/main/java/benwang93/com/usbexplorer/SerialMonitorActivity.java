216362e76ca39442d8df7bfcf2273c86deda96e9
Ben Wang <benwang@umich.edu>
2015-08-12 20:50:50 -0400

    Restructured some code and implementing receive now

--- app/src/main/java/benwang93/com/usbexplorer/SerialMonitorActivity.java
+++ app/src/main/java/benwang93/com/usbexplorer/SerialMonitorActivity.java
@@ -8,6 +8,7 @@ import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbEndpoint;
 import android.hardware.usb.UsbInterface;
 import android.hardware.usb.UsbManager;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
@@ -22,7 +23,12 @@ import android.widget.Toast;
 public class SerialMonitorActivity extends AppCompatActivity {
     // USB device/manager
     private UsbManager mUsbManager;
-    UsbDevice device;
+    private UsbDevice device;
+
+    private UsbInterface intf;
+    private UsbEndpoint endpointSend;
+    private UsbEndpoint endpointReceive;
+    private UsbDeviceConnection connection;
 
     // Variables for sending via USB
     private static int TIMEOUT = 0;
@@ -44,6 +50,11 @@ public class SerialMonitorActivity extends AppCompatActivity {
         // Get USB manager for send/receive
         mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
 
+        intf = device.getInterface(1);
+//        endpointReceive = intf.getEndpoint(1);
+        endpointSend = intf.getEndpoint(0);
+        connection = mUsbManager.openDevice(device);
+
         // UI elements
         textViewSM = (TextView) findViewById(R.id.textViewSerialMonitor);
         textViewSM.setMovementMethod(new ScrollingMovementMethod());
@@ -55,21 +66,10 @@ public class SerialMonitorActivity extends AppCompatActivity {
         findViewById(R.id.buttonSend).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
+                // Send and display message
                 sendSerial(editTextSM.getText().toString());
-                textViewSM.append("\n"+editTextSM.getText());
+                displayMessage(textViewSM, "\n" + editTextSM.getText());
                 editTextSM.setText("");
-
-                // find the amount we need to scroll.  This works by
-                // asking the TextView's internal layout for the position
-                // of the final line and then subtracting the TextView's height
-                final int scrollAmount = textViewSM.getLayout().getLineTop(textViewSM.getLineCount()) - textViewSM.getHeight();
-                // if there is no need to scroll, scrollAmount will be <=0
-                if (scrollAmount > 0)
-                    textViewSM.scrollTo(0, scrollAmount);
-                else
-                    textViewSM.scrollTo(0, 0);
             }
         });
 
@@ -129,30 +129,82 @@ public class SerialMonitorActivity extends AppCompatActivity {
         bytes = message.getBytes();
 
         // DEBUG: Show message being sent
-        Toast.makeText(getApplicationContext(), "Sending: " + bytes, Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Sending: " + new String(bytes), Toast.LENGTH_SHORT).show();
 
         // Send text over serial
-        UsbInterface intf = device.getInterface(1);
-        UsbEndpoint endpoint = intf.getEndpoint(0);
-        UsbDeviceConnection connection = mUsbManager.openDevice(device);
         connection.claimInterface(intf, forceClaim);
-        connection.bulkTransfer(endpoint, bytes, bytes.length, TIMEOUT); //do in another thread
+        connection.bulkTransfer(endpointSend, bytes, bytes.length, TIMEOUT); //do in another thread
     }
 
     private void debugPrint(){
 //        textViewSM.append("\ndebugPrint() says hello world!");
-        textViewSM.append("\nNum USB interfaces: " + device.getInterfaceCount());
+        displayMessage(textViewSM, "\nNum USB interfaces: " + device.getInterfaceCount());
 
         for (int intNum = 0; intNum < device.getInterfaceCount(); ++ intNum){
             UsbInterface intf = device.getInterface(intNum);
 
-            textViewSM.append("\nScanning interface " + intNum + " for endpoints");
+            displayMessage(textViewSM, "\n\nScanning interface " + intNum + " for endpoints");
 
             for (int endptNum = 0; endptNum < intf.getEndpointCount(); ++endptNum){
                 UsbEndpoint endpt = intf.getEndpoint(endptNum);
 
-                textViewSM.append("\n\t"+endpt.toString());
+                displayMessage(textViewSM, "\n  " + endpt.toString());
             }
+        }
+    }
+
+    private void displayMessage(TextView textView, String message){
+        // Append text
+        textView.append(message);
+
+        // find the amount we need to scroll.  This works by
+        // asking the TextView's internal layout for the position
+        // of the final line and then subtracting the TextView's height
+
+        int scrollAmount;
+
+        // Calculate scroll amount
+        if (textView.getLineCount() == 0)
+            scrollAmount = 0;
+        else
+            scrollAmount = textView.getLayout().getLineTop(textView.getLineCount()) - textView.getHeight();
+
+        // if there is no need to scroll, scrollAmount will be <=0
+        if (scrollAmount > 0)
+            textView.scrollTo(0, scrollAmount);
+        else
+            textView.scrollTo(0, 0);
+    }
+
+    private boolean mRunning = false;
+    private class SerialReceiveTask extends AsyncTask<Void, String, Integer> {
+        @Override
+        protected Integer doInBackground(Void... endpt) {
+            mRunning = true;
+
+            //READ VALUE UNTIL DISCONNECT
+            while (mRunning) {
+
+                byte[] bytes = new byte[endpointReceive.getMaxPacketSize()];
+                int result = connection.bulkTransfer(endpointReceive, bytes, bytes.length, 1000);
+                if (result > 0) {
+//                    Log.e("RESULT : " + result, "  VALUE : " + new String(bytes));
+                    publishProgress(new String(bytes));
+
+                }
+            }
+
+            return 0;
+        }
+
+        protected void onProgressUpdate(String message) {
+            // Show message in textViewSM
+            displayMessage(textViewSM, message);
+        }
+
+        protected void onPostExecute(Integer status) {
+            // Show message in textViewSM
+            displayMessage(textViewSM, "Error: Connection closed. Exit status: " + status);
         }
     }
 }
